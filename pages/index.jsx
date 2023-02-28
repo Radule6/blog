7aa013351d3a8a8b839492d0d98aa62cfb981493
Marko Radulovic <marko.radulovic04@gmail.com>
2023-02-28 12:44:14 +0100

    added the initial header with hardcoded stuff

---
+++ pages/index.jsx
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import { PostCard, PostWidget, Categories } from "../components";
+
+const posts = [
+  {
+    title: "NextJS Testing",
+    excerpt: "Learn nextjs",
+  },
+  { title: "React Testing", excerpt: "Learn react" },
+  { title: "React with Tailwind", excerpt: "Learn react with tailwind" },
+];
+
+const Home = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
